f64932f9d6c562f204bb5b8888fe2c8df3315e09
Vansh Bansal <vb.vansh04@gmail.com>
2025-07-22 00:50:41 +0530

    Initial project setup

---
+++ src/main/java/com/spookzie/BookMyEvent/BookMyEventApplication.java
@@ -0,0 +1,13 @@
+package com.spookzie.BookMyEvent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMyEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMyEventApplication.class, args);
+	}
+
+}
